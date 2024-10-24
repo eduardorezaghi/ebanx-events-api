cbc4a2ca7f5536aaff1dadcf189cb4ac933444e7
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-10-24 20:33:28 -0300

    Move base repository to global variable

--- src/dependencies/repositories.py
+++ src/dependencies/repositories.py
@@ -1,13 +1,15 @@
 from decimal import Decimal
 
-from src.models.balance import Balance
+from src.models import Balance
 from src.repositories import BalanceRepository
+
+global_balance_repo = BalanceRepository(
+    [
+        Balance(account_id=100, balance=Decimal("99.95")),
+        Balance(account_id=200, balance=Decimal("201.28")),
+    ]
+)
 
 
 def get_balance_repository():
-    yield BalanceRepository(
-        [
-            Balance(account_id=1, balance=Decimal("99.95")),
-            Balance(account_id=2, balance=Decimal("201.28")),
-        ]
-    )
+    yield global_balance_repo
