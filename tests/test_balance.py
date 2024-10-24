c98d6964d3ed7284cc6f8a5177a08b2eb93cd20e
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-10-24 17:27:47 -0300

    Implement /balance endpoint tests

--- tests/test_balance.py
+++ tests/test_balance.py
@@ -1,20 +1,42 @@
 import http
+from decimal import Decimal
 
+import pytest
+
+from src import main
+from src.dependencies.repositories import get_balance_repository
+from src.models import Balance
+from src.repositories import BalanceRepository
 from tests.conftest import HttpTests
 
 
+@pytest.fixture
+def balance_repo():
+    yield BalanceRepository(
+        [
+            Balance(account_id=1, balance=Decimal("89.95")),
+            Balance(account_id=2, balance=Decimal("201.28")),
+        ]
+    )
+
+
 class TestBalance(HttpTests):
+    @pytest.fixture(autouse=True)
+    def setup_method(self, balance_repo: BalanceRepository):
+        main.app.dependency_overrides[get_balance_repository] = lambda: balance_repo
+
     def test_query_balance(self):
         # Act
-        response = self.client.get("/balance/", params={"account_id": 1})
+        response = self.client.get("/balance", params={"account_id": 1})
 
         # Assert
-        assert response.status_code == 200
-        assert response.json() is None
+        assert response.status_code == http.HTTPStatus.OK.value
+        assert response.json() == "89.95"
 
     def test_unexisting_balance_should_return_404(self):
         # Act
-        response = self.client.get("/balance/", params={"account_id": 999})
+        response = self.client.get("/balance", params={"account_id": 999})
 
         # Assert
         assert response.status_code == http.HTTPStatus.NOT_FOUND.value
+        assert response.json() == 0
