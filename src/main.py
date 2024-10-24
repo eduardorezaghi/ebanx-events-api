38be5dddd6f6a46bbb347651022aa6f546799be0
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-10-24 10:19:17 -0300

    Add /reset endpoint to clear application data

--- src/main.py
+++ src/main.py
@@ -5,9 +5,18 @@ from fastapi import APIRouter, Depends, FastAPI
 from src.api import balances, events
 
 
+base_router = APIRouter(
+    tags=["base"],
+)
+
+@base_router.post("/reset", status_code=200)
+async def reset_api_data():
+    return None
+
 def create_application() -> FastAPI:
     app = FastAPI()
 
+    app.include_router(base_router)
     app.include_router(events.router)
     app.include_router(balances.router)
 
