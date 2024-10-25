8cfdf1f6472658c3a11cb87fc2b68debc70bb98b
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-10-24 22:13:48 -0300

    Use clean state for repository global dependency

--- src/dependencies/repositories.py
+++ src/dependencies/repositories.py
@@ -1,14 +1,6 @@
-from decimal import Decimal
-
-from src.models import Balance
 from src.repositories import BalanceRepository
 
-global_balance_repo = BalanceRepository(
-    [
-        Balance(account_id=100, balance=Decimal("99.95")),
-        Balance(account_id=200, balance=Decimal("201.28")),
-    ]
-)
+global_balance_repo = BalanceRepository()
 
 
 def get_balance_repository():
